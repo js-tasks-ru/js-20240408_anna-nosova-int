685c497f66e1b3ac7d7c5390436edee3271db0d8
AnnaNosova <anna.nosova@masterdata.ru>
2024-05-06 00:11:41 +0300

    hw 5.1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,44 @@
 export default class NotificationMessage {
+  element;
 
+  constructor(
+    title = '',
+    { duration = 0,
+      type = ''
+    } = {}) {
+    this.duration = duration;
+    this.type = type;
+    this.title = title;
+
+    this.element = this.createTemplate();
+  }
+
+  createTemplate() {
+    const element = document.createElement('div');
+
+    element.innerHTML = `<div class="notification ${this.type}" style="--value:${this.duration}ms">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.title}
+        </div>
+      </div>
+    </div>`;
+    return element.firstElementChild;
+  }
+
+  show(container = document.body) {
+    container.append(this.createTemplate());
+
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
