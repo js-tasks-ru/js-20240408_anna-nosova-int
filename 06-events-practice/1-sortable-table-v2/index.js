4870c889d024719737e56610dd394bc10a2aa0bc
AnnaNosova <anna.nosova@masterdata.ru>
2024-06-03 20:41:28 +0300

    hw 6.1

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,25 @@
-export default class SortableTable {
+import SortableTableV1 from '../../05-dom-document-loading/2-sortable-table-v1/index.js';
+
+export default class SortableTable extends SortableTableV1 {
   constructor(headersConfig, {
     data = [],
-    sorted = {}
+    sorted = {},
   } = {}) {
+    super(headersConfig, data);
+    this.sorted = sorted;
+    this.element = this.createElement(this.createTemplate());
 
+    this.sort(this.sorted.id, this.sorted.order);
+    this.onHeaderColClick();
+  }
+
+  onHeaderColClick() {
+    const header = this.element.querySelector(`[data-element ="header"]`);
+    header.onpointerdown = (event) => {
+      const col = event.target.closest(`[data-sortable ="true"]`);
+      const sortOrder = col.dataset.order === 'asc' ? 'desc' : 'asc';
+
+      this.sort(col.dataset.id, sortOrder);
+    };
   }
 }
