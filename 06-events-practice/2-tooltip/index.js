b5c03f14811251d619e2d888c1b68407a871ca55
AnnaNosova <anna.nosova@masterdata.ru>
2024-06-05 17:25:49 +0300

    hw 6.2

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,124 @@
 class Tooltip {
-  initialize () {
+  element;
+  static #instance = null;
 
+  constructor() {
+    if (!Tooltip.#instance) {
+      Tooltip.#instance = this;
+    } else {
+      return Tooltip.#instance;
+    }
+  }
+
+  createElement() {
+    const tooltipElem = document.createElement('div');
+    tooltipElem.className = 'tooltip';
+    return tooltipElem;
+  }
+
+  render(textContent) {
+    this.element.textContent = textContent;
+    document.body.append(this.element);
+  }
+
+  addEventListeners() {
+    document.addEventListener('pointerover', this.onPointerOver);
+    document.addEventListener('pointermove', this.onPointerMove);
+    document.addEventListener('pointerout', this.onPointerOut);
+  }
+
+  onPointerOver = event => {
+    if (event.target.dataset.tooltip) {
+      this.render(event.target.dataset.tooltip);
+    }
+  }
+
+  onPointerMove = event => {
+    if (event.target.dataset.tooltip) {
+      this.element.style.left = event.clientX + 'px';
+      this.element.style.top = event.clientY + 'px';
+    }
+  }
+
+  onPointerOut = (event) => {
+    if (event.target.dataset.tooltip) {
+      this.element.remove();
+    }
+  }
+
+  initialize () {
+    this.element = this.createElement();
+    this.addEventListeners();
+  }
+
+  removeEventListeners() {
+    document.removeEventListener('pointerover', this.onPointerOver);
+    document.removeEventListener('pointermove', this.onPointerMove);
+    document.removeEventListener('pointerout', this.onPointerOut);
+  }
+
+  destroy() {
+    this.element.remove();
+    this.removeEventListeners();
   }
 }
 
 export default Tooltip;
+
+// class Tooltip {
+//  element;
+//
+//   constructor() {
+//   }
+//
+//   createElement() {
+//     const tooltipElem = document.createElement('div');
+//     tooltipElem.className = 'tooltip';
+//     return tooltipElem;
+//   }
+//
+//   addEventListeners() {
+//     document.addEventListener('pointerover', this.onPointerOver);
+//     document.addEventListener('pointermove', this.onPointerMove);
+//     document.addEventListener('pointerout', this.onPointerOut);
+//   }
+//
+//   onPointerOver = event => {
+//     if (event.target.dataset.tooltip) {
+//       this.element.innerHTML = event.target.dataset.tooltip;
+//       document.body.append(this.tooltip);
+//     }
+//   }
+//
+//   onPointerMove = event => {
+//     if (event.target.dataset.tooltip) {
+//       this.element.style.left = event.clientX + 'px';
+//       this.element.style.top = event.clientY + 'px';
+//     }
+//   }
+//
+//   onPointerOut = event => {
+//     if (event.target.dataset.tooltip) {
+//       this.element.remove();
+//     }
+//   }
+//
+//
+//   initialize () {
+//     this.element = this.createElement();
+//     this.addEventListeners();
+//   }
+//
+//   removeEventListeners() {
+//     document.removeEventListener('pointerover', this.onPointerOver);
+//     document.removeEventListener('pointermove', this.onPointerMove);
+//     document.removeEventListener('pointerout', this.onPointerOut);
+//   }
+//
+//   destroy() {
+//     this.element.remove();
+//     this.removeEventListeners();
+//   }
+// }
+//
+// export default Tooltip;
